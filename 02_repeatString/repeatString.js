894496229369778b54fcf71168afdbc5a57777fe
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-04-15 20:54:02 +0200

    solve exercise 01 - 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number < 0) {
+    return "ERROR";
+  }
+  let repStr = "";
+  for (let n = 0; n < number; n++) {
+    repStr += string;
+  }
+  return repStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  for (const element of args) {
+    let index = array.indexOf(element);
+    while (index > -1) {
+      array.splice(index, 1);
+      index = array.indexOf(element);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
