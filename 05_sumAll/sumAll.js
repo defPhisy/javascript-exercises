484212c7cb7c3b3f908e57a53dd87980580e7a5c
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-04-16 20:09:17 +0200

    solve exercise 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start > end) {
+    const temp = end;
+    end = start;
+    start = temp;
+  }
+  if (
+    typeof start !== 'number' ||
+    typeof end !== 'number' ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
 
+  let result = 0;
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
