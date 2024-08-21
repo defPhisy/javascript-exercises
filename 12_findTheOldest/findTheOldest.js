5add5483414b7bb9553d00a71609e0faa52f6244
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-08-21 12:36:46 +0200

    💥 solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+    return oldestAge > currentAge ? oldest : current;
+  });
 };
+
+const getAge = function (death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
