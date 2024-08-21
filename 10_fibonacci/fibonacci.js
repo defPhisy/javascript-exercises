31a3602b440ef2293e411985a0023c9c311a06a9
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-08-21 11:25:52 +0200

    💥 solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibSequence = [0, 1, 1, 2, 3, 5, 8];
 
+const fibonacci = function (n) {
+  // if (n != Math.abs(n)) {
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  while (fibSequence[n] === undefined) {
+    const lastIndex = fibSequence.length - 1;
+    fibSequence.push(fibSequence[lastIndex - 1] + fibSequence[lastIndex]);
+  }
+  return fibSequence[n];
 };
 
 // Do not edit below this line
