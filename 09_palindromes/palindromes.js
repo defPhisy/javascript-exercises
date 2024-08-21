641da917a8ecf7f276aea4262e0da27bf2dadf92
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-08-21 10:19:58 +0200

    💥 solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.replace(/[^a-z0-9]/gi,"").toLowerCase();
+  const reverseStr = cleanStr.split("").reverse().join("");
+  return reverseStr === cleanStr;
 };
 
 // Do not edit below this line
